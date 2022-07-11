72b42aa274182f438f09d6e5dd3eb237bc9e6c5e
Andrew Chen <43127177+achen4824@users.noreply.github.com>
2022-07-11 13:52:05 +0930

    Designed Schema starting on Classes for Data Representation

---
+++ src/test/java/org/technology/consilium/ApplicationTest.java
@@ -0,0 +1,13 @@
+package org.technology.consilium;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment= SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
